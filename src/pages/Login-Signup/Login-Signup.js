fc3aaf51e115512c9182a9ef63524d7c21cef044
MukulRane <rane.mukul@gmail.com>
2023-06-25 15:08:50 -0700

    Login/Signup page without css and validation

--- src/pages/Login-Signup/Login-Signup.js
+++ src/pages/Login-Signup/Login-Signup.js
@@ -1,7 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Login-Signup.css";
 
 const LoginSignup = () => {
-  return <div>LoginSignup Page</div>;
+  const [authMode, setAuthMode] = useState("signin");
+
+  const changeAuthMode = () => {
+    setAuthMode(authMode === "signin" ? "signup" : "signin");
+  };
+
+  if (authMode === "signin") {
+    return (
+      <div className="Login-Signup-Auth-form-container">
+        <form className="Login-Signup-Auth-form">
+          <div className="Login-Signup-Auth-form-content">
+            <h3 className="Login-Signup-Auth-form-title">Sign In</h3>
+            <div className="text-center">
+              Not registered yet?{" "}
+              <span className="link-primary" onClick={changeAuthMode}>
+                Sign Up
+              </span>
+            </div>
+            <div className="form-group">
+              <label>Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Enter email"
+              />
+            </div>
+            <div className="form-group">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Enter password"
+              />
+            </div>
+            <div className="d-grid gap-2">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+            <p className="text-center mt-2">
+              Forgot <a href="#">password?</a>
+            </p>
+          </div>
+        </form>
+      </div>
+    );
+  }
+
+  return (
+    <div className="Login-Signup-Auth-form-container">
+      <form className="Login-Signup-Auth-form">
+        <div className="Login-Signup-Auth-form-content">
+          <h3 className="Login-Signup-Auth-form-title">Sign Up</h3>
+          <div className="text-center">
+            Already registered?{" "}
+            <span className="link-primary" onClick={changeAuthMode}>
+              Sign In
+            </span>
+          </div>
+          <div className="form-group">
+            <label>Full Name</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="e.g Jane Doe"
+            />
+          </div>
+          <div className="form-group">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Email Address"
+            />
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+            />
+          </div>
+          <div className="d-grid gap-2">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          <p className="text-center mt-2">
+            Forgot <a href="#">password?</a>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default LoginSignup;
